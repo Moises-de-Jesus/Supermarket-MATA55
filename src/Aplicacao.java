0ebdf3e4fd8f344549d9b1f54b8cdd98d1788658
IcaroS299 <icarosacramento02@gmail.com>
2026-05-25 09:26:23 -0300

    feat: Add admitir method in Aplicacao

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 class Aplicacao{
-    public Aplicacao{
+    private static ArrayList<Funcionario> listaFuncionarios = new ArrayList<Funcionario>()
+    public static void main(String[] args){
+    
+
+    
+    }
+
+    public Funcionario admitir(String nome, String cpf, String salario){
         
     }
 }
